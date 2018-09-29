8acec79404d242d2b1ecccb9c3134b8f075e5823
DeepInThought <support@deepinthought.io>
2018-09-29 16:53:20 -0500

    [Git Commit]: at 2018-09-29 16:53:20-05:00

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ubuntu:latest as ubuntu_builder
 RUN apt-get -y update && apt-get install curl -y
 
-FROM deepinthought/vscode-shell-snippets
+### FROM deepinthought/vscode-shell-snippets:latest
 COPY . /vscode-shell-snippets
 LABEL Name=vscode-shell-snippets Version=0.1.0
 CMD [ "/bin/bash", "cd /vscode-shell-snippets" ]
---
+++ docker-builder.sh.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+### @File   : docker-builder.sh.sh
+### @Author : Luke Penrod - support@deepinthought.io
+### @Company : DeepInThought
+### @Link   : https://deepinthought.io
+### @Date   : Saturday,September 29 2018 01:53:45
+###-
+
+docker build $PWD/ -t deepinthought/vscode-shell-snippets:latest
+docker push deepinthought/vscode-shell-snippets:latest
