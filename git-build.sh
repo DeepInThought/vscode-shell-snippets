1130fb8b8f358b18472a63f4579245bfa5b05e94
DeepInThought <support@deepinthought.io>
2018-09-29 15:12:18 -0500

    [Git Commit]: at 2018-09-29 15:12:18-05:00

--- git-build.sh
+++ git-build.sh
@@ -23,41 +23,62 @@ __with_extension="${__base}.sh"
 handle_exit_code() {
 	ERROR_CODE="$?"
 	if [ "${ERROR_CODE}" = "0" ]; then
-		printf -- '\033[32m SUCCESS: %s \033[0m\n' "${deepMsg:-${__with_extension}}";
-        printf -- '\033[32m Finished without errors. \033[0m\n'
-        printf -- '\033[32m Exiting with status code 0.\033[0m\n';
+		printf -- '\033[32m [SUCCESS]: %s \033[0m\n' "${deepMsg:-${__with_extension}}"
+		printf -- '\033[32m [FINISHED] without errors. \033[0m\n'
+		printf -- '\033[32m [EXIT] with status code %s.\033[0m\n' "${ERROR_CODE}"
+		exit ${ERROR_CODE}
+	fi
+	if [ "${ERROR_CODE}" = "1" ]; then
+		printf -- '\033[31m [ERROR CODE %s]: ERROR_CODE %s primary to echo call failed.\033[0m\n' ${ERROR_CODE} ${ERROR_CODE}
+		printf -- '\033[31m [EXIT] with status code %s.\033[0m\n' "${ERROR_CODE}"
+		exit ${ERROR_CODE}
+	fi
+	if [ "${ERROR_CODE}" = "2" ]; then
+		printf -- '\033[31m [ERROR CODE %s]: ERROR_CODE %s primary to echo call failed.\033[0m\n' ${ERROR_CODE} ${ERROR_CODE}
+		printf -- '\033[31m [EXIT] with status code %s.\033[0m\n' "${ERROR_CODE}"
 		exit ${ERROR_CODE}
 	fi
 	# ...
 	if [ "${ERROR_CODE}" != "0" ]; then
-		printf -- 'Exiting with status code 2.\n'
+		printf -- '\033[31m [ERROR CODE %s]: ERROR_CODE %s is not defined yet.\033[0m\n' ${ERROR_CODE} ${ERROR_CODE}
+		printf -- '\033[31m [EXIT] with status code %s.\033[0m\n' "${ERROR_CODE}"
 		exit ${ERROR_CODE}
 	fi
-	printf -- "an error occurred. cleaning up now... "
-	# ... cleanup code ...
-	printf -- "DONE.\\nExiting with error code %s.\\n", "${ERROR_CODE}"
+	printf -- '\033[31m [ERROR] occurred.  Cleaning up now...\033[0m\n'
+	### Cleanup
+    printf -- '\033[33m [CLEAN]: Cleaning exit properly.\n\033[31m [EXIT] with error code %s.\n' "${ERROR_CODE}"
 	exit "${ERROR_CODE}"
 }
 trap "handle_exit_code" EXIT
 
+if [ -d "${GIT_RAW_TEST}" ]; then
+	echo "[Testing]: ${GIT_RAW_TEST} exists."
+	curl "${GIT_RAW_TEST}" && echo || exit 2
+    printf -- '\033[32m [SUCCESS]: %s \033[0m\n' ${GIT_RAW_TEST}
+	sleep 1
+else
+	echo "[GIT_RAW_TEST]: Not Setup! ${GIT_RAW_TEST}"
+	git clone "${ROOT_PATH}/${GIT_REPOSITORY}" && echo || exit 2
+	printf -- '\033[32m [SUCCESS]: %s \033[0m\n' "${GIT_LOCAL_PATH}"
+	sleep 1
+fi
 if [ $# -eq 1 ]; then
-	"${GIT_COMMIT_MSG}"="$1"
+	${GIT_COMMIT_MSG}="$1"
 	${GIT_REPOSITORY:-'https://github.com/DeepInThought/vscode-shell-snippets.git'}
-	echo "[Git Comment]: ${GIT-COMMIT}"
+	echo "[Git Comment]: ${GIT-GIT_COMMIT_MSG}"
 	echo "[Git Repository]: ${GIT_REPOSITORY}"
-	if [ -d "${GIT_RAW_TEST}" ]; then
-		echo "[Testing]: ${GIT_RAW_TEST} exists."
-        curl "${GIT_RAW_TEST}" && echo 
-		sleep 1
-	else
-		echo "[GIT_RAW_TEST]: Not Setup! ${GIT_RAW_TEST}"
-		git clone "${ROOT_PATH}/${GIT_REPOSITORY}" 
-		sleep 1
-	fi
+    git commit -m ${GIT_COMMIT_MSG} || printf -- '\033[32m [EXIT] with status code %s.\033[0m\n' ${ERROR_CODE} && exit 1
+    printf -- '\033[32m [Git Commit]: [FINISHED] without errors. \033[0m\n'
+else
+    ${GIT_COMMIT_MSG:-"echo [Git Commit]: on $(date --rfc-3339=seconds)"}
+    git commit -m ${GIT_COMMIT_MSG} || printf -- '\033[32m [EXIT] with status code %s.\033[0m\n' ${ERROR_CODE} && exit 1
+    printf -- '\033[32m [Git Commit]: [FINISHED] without errors. \033[0m\n'
+fi
+if [ $# -eq 2 ]; then
 	echo "[Git Comment]: ${GIT-COMMIT}"
-    echo "Commit on: $(date --rfc-3339=seconds)"
-    git commit -m ${GIT_COMMIT_MSG} || printf -- '\033[31m ERROR: git commit -m ${GIT_COMMIT_MSG} failed! \033[0m\n' && exit 1
-    printf -- '\033[32m Finished without errors. \033[0m\n'
+	echo "Commit on: $(date --rfc-3339=seconds)"
+	git commit -m ${GIT_COMMIT_MSG} || printf -- '\033[32m [EXIT] with status code %s.\033[0m\n' ${ERROR_CODE} && exit 1
+	printf -- '\033[32m [Git Commit]: [FINISHED] without errors. \033[0m\n'
 else
 	echo "Commit on: $(date --rfc-3339=seconds)" && echo "${GIT_COMMIT_MSG}"
 fi
